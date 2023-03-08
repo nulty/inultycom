e6b6c2525cbd0c0b1866525971bcc1e6bf1565b7
Iain McNulty <iain@inulty.com>
2023-03-08 23:28:58 +0000

    Add templates, views and urls

--- main/views.py
+++ main/views.py
@@ -1,5 +1,19 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    page_name = "home"
+    template = loader.get_template("main/home.html")
+    context = {
+        "page_name": page_name,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def work(request):
+    page_name = "work"
+    template = loader.get_template("main/work.html")
+    context = {"page_name": page_name, "years": 9}
+    return HttpResponse(template.render(context, request))
