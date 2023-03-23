63c28b848273a42fff5597a3c93f2e11a048c360
Iain McNulty <iain@inulty.com>
2023-03-23 16:06:00 +0000

    Set Google Secrets manager as source for SECRET_KEY

--- inultycom/settings.py
+++ inultycom/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import environ
 import os
+import io
+from google.cloud import secretmanager
 
 env = environ.Env(
     # set casting, default value
@@ -23,7 +25,17 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_file = os.path.join(BASE_DIR, ".env")
 
-if os.path.isfile(env_file):
+if os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    # Pull secrets from Secret Manager
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+    client = secretmanager.SecretManagerServiceClient()
+    settings_name = os.environ.get("SETTINGS_NAME")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+
+    env.read_env(io.StringIO(payload))
+elif os.path.isfile(env_file):
     # Use a local secret file, if provided
 
     env.read_env(env_file)
@@ -37,16 +49,6 @@ elif os.getenv("TRAMPOLINE_CI", None):
     )
     env.read_env(io.StringIO(placeholder))
 # [END_EXCLUDE]
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-    env.read_env(io.StringIO(payload))
 else:
     raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 # [END gaestd_py_django_secret_config]
