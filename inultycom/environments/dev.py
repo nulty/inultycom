70f055549cd03e892dd65ff54f0e04979e8f6679
Iain McNulty <iain@inulty.com>
2023-04-03 16:54:27 +0100

    Setup dev and prod envs to use .env

--- inultycom/environments/dev.py
+++ inultycom/environments/dev.py
@@ -20,9 +20,6 @@ SECRET_KEY = env("SECRET_KEY")
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": env.db()
+}
